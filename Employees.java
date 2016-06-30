e85f3869488925a88d81f3308ed3ad1c853df94b
akashgoyal0447 <goyal.akash9999@gmail.com>
2016-06-30 13:15:09 +0530

    Create Employees.java

---
+++ Employees.java
@@ -0,0 +1,42 @@
+package com.tcs.myapplication;
+
+
+public class Employees {
+
+    private String mID;
+    private String mName;
+    private String mDept;
+    private String mPhone;
+
+    public String getmID() {
+        return mID;
+    }
+
+    public void setmID(String mID) {
+        this.mID = mID;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getmDept() {
+        return mDept;
+    }
+
+    public void setmDept(String mDept) {
+        this.mDept = mDept;
+    }
+
+    public String getmPhone() {
+        return mPhone;
+    }
+
+    public void setmPhone(String mPhone) {
+        this.mPhone = mPhone;
+    }
+}
